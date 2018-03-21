7b189da478879d64bad645c6f6acc5249ff133fe
Cameron Smith <gitcontrol@outlook.com>
2018-03-21 21:43:29 +0000

    Add doc blocks

--- src/Behavioral/NullObject/NoPlayer.php
+++ src/Behavioral/NullObject/NoPlayer.php
@@ -5,21 +5,41 @@ use UKCASmith\DesignPatterns\Behavioral\NullObject\Contracts\PlayerMovement;
 class NoPlayer implements PlayerMovement
 {
 
+    /**
+     * Turn player right.
+     *
+     * @return null
+     */
     public function right()
     {
         return null;
     }
 
+    /**
+     * Turn player left.
+     *
+     * @return null
+     */
     public function left()
     {
         return null;
     }
 
+    /**
+     * Move player top.
+     *
+     * @return null
+     */
     public function top()
     {
         return null;
     }
 
+    /**
+     * Move player down.
+     *
+     * @return null
+     */
     public function bottom()
     {
         return null;
--- src/Behavioral/NullObject/PlayerOne.php
+++ src/Behavioral/NullObject/PlayerOne.php
@@ -5,21 +5,41 @@ use UKCASmith\DesignPatterns\Behavioral\NullObject\Contracts\PlayerMovement;
 class PlayerOne implements PlayerMovement
 {
 
+    /**
+     * Do no action.
+     *
+     * @return string
+     */
     public function right()
     {
         return 'right';
     }
 
+    /**
+     * Do no action.
+     *
+     * @return string
+     */
     public function left()
     {
         return 'left';
     }
 
+    /**
+     * Do no action.
+     *
+     * @return string
+     */
     public function top()
     {
         return 'top';
     }
 
+    /**
+     * Do no action.
+     *
+     * @return string
+     */
     public function bottom()
     {
         return 'bottom';
